36866b7aeaf3978d70c4605526ec5185f81a5ced
Philipp Kapfer <philipp.kapfer@gmx.at>
2014-03-13 22:40:30 +0100

    Use Roundcube DB factory instead of MDB2 (which isn't available anymore in version 1.0) Read and inherit purge values from domains in addition to single mailboxes Allow hiding the settings tab if users shouldn't be able to configure limits themselves
    Refactored purge script:
    Initialize Roundcube class to access logs and database via it's methods instead of manually
    Use maildir path from database instead of manually constructing it

--- purgefolders.php
+++ purgefolders.php
@@ -14,72 +14,103 @@
 
 */
 
-require_once 'PEAR.php';
-require_once 'MDB2.php';
-require_once("config.inc.php");
+define('INSTALL_PATH', __DIR__ . '/../../');
+require_once INSTALL_PATH . 'program/include/iniset.php';
 
-$log_file = "/var/log/roundcube/purgefolders.log";
+$rcmail = rcmail::get_instance();
+$rcmail->plugins->load_plugin('purge', true);
+$rcmail->write_log('purgefolders', 'Purge operation started');
 
-$log = fopen($log_file, "a");
-fwrite($log, date('Y-m-d H:i:s') . " - Purge operation started.\n");
-
-$options = array(
-	'persistent' => true
-	);
-
-$db = MDB2::connect($rcmail_config['purge_sql_dsn'], $options);
-if (PEAR::isError($db)) {
-	exit($db->getMessage());
+if ($dsn = $rcmail->config->get('purge_sql_dsn')) {
+	if (is_array($dsn) && empty($dsn['new_link'])) {
+		$dsn['new_link'] = true;
+	}
+	else if (!is_array($dsn) && !preg_match('/\?new_link=true/', $dsn)) {
+		$dsn .= '?new_link=true';
+		}
+	$db = rcube_db::factory($dsn, '', false);
+	$db->set_debug((bool)$rcmail->config->get('sql_debug'));
+	$db->db_connect('w');
+}
+else {
+	$db = $rcmail->get_dbh();
 	}
 
-$sql_result = $db->query($rcmail_config['purge_script_query']);
-if (PEAR::isError($sql_result)) {
-	exit($db->getMessage());
+if ($err = $db->is_error()) {
+	$rcmail->write_log('purgefolders', 'Error connecting to database: ' . $err);
+	exit($err . "\n");
 	}
 
-$mailboxes = $sql_result->fetchAll(MDB2_FETCHMODE_ASSOC);
+$sql_result = $db->query($rcmail->config->get('purge_script_query'));
+if ($err = $db->is_error()) {
+	$rcmail->write_log('purgefolders', 'Error reading mailboxes from database: ' . $err);
+	exit($err . "\n");
+	}
 
+$mailboxes = $sql_result->fetchAll();
 for ( $i = 0 ; $i < count($mailboxes) ; $i++) {
+	$purge_trash = $mailboxes[$i]['purge_trash'];
+	$purge_junk = $mailboxes[$i]['purge_junk'];
+	if (($purge_trash === null || $purge_junk === null) && $rcmail->config->get('purge_sql_read_domain') !== null) {
+		$search = array(
+				'%domain'
+				);
+		$replace = array(
+				$db->quote($mailboxes[$i]['domain'])
+				);
+		$query = str_replace($search, $replace, $rcmail->config->get('purge_sql_read_domain'));
+		$sql_result = $db->query($query);
+		if ($err = $db->is_error()) {
+			$rcmail->write_log('purgefolders', 'Error reading domain quota for ' . $mailboxes[$i]['domain'] . ': ' . $err);
+			exit($err . "\n");
+		    }
+		$sql_arr = $db->fetch_assoc($sql_result);
+
+		if ($purge_trash === null) {
+			$purge_trash = $sql_arr['purge_trash'];
+			}
+		if ($purge_junk === null) {
+			$purge_junk = $sql_arr['purge_junk'];
+			}
+		}
+	$purge_trash = intval($purge_trash);
+	$purge_junk = intval($purge_junk);
+
 	$tcount = 0;
-	$tlast = intval($mailboxes[$i]['purge_trash']) > 0 ? (intval($mailboxes[$i]['purge_trash']) == 1 ? " 1 day" : " " . strval($mailboxes[$i]['purge_trash']) . " days") : "ever";
-	fwrite($log, date('Y-m-d H:i:s') . "     User " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . " keeps messages in Trash folder for" . $tlast . ".\n");
-	if (intval($mailboxes[$i]['purge_trash']) > 0) {
-		$trash = $rcmail_config['purge_maildir_path'] . "/" . $mailboxes[$i]['domain'] . "/" . $mailboxes[$i]['local_part'] . "/Maildir/.Trash/cur";
+	$tlast = $purge_trash > 0 ? ($purge_trash == 1 ? " 1 day" : " " . strval($purge_trash) . " days") : "ever";
+	$rcmail->write_log('purgefolders', "User " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . " keeps messages in Trash folder for" . $tlast);
+	if ($purge_trash > 0) {
+		$trash = $rcmail->config->get('purge_maildir_path') . "/" . $mailboxes[$i]['maildir'] . "/.Trash/cur";
 		$files = array_diff(scandir($trash), array('..', '.'));
 		if (!empty($files)) {
 			foreach ($files as $filename) {
-				if ((time() - filemtime($trash . "/" . $filename)) > ($mailboxes[$i]['purge_trash'] * 60 * 60 * 24)) {
-					fwrite($log, date('Y-m-d H:i:s') . "     Purging file " . $filename . " (dated " . date('Y-m-d H:i:s', filemtime($trash . "/" . $filename)) . ") from Trash folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . ".\n");
+				if ((time() - filemtime($trash . "/" . $filename)) > ($purge_trash * 60 * 60 * 24)) {
+					$rcmail->write_log('purgefolders', "Purging file " . $filename . " (dated " . date('Y-m-d H:i:s', filemtime($trash . "/" . $filename)) . ") from Trash folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain']);
 					unlink($trash . "/" . $filename);
 					$tcount++;
 					}
 				}
 			}
 		}
-	fwrite($log, date('Y-m-d H:i:s') . "       " . $tcount . " messages purged from Trash folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . ".\n");
+	$rcmail->write_log('purgefolders', $tcount . " messages purged from Trash folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain']);
 	$jcount = 0;
-	$jlast = intval($mailboxes[$i]['purge_junk']) > 0 ? (intval($mailboxes[$i]['purge_junk']) == 1 ? " 1 day" : " " . strval($mailboxes[$i]['purge_junk']) . " days") : "ever";
-	fwrite($log, date('Y-m-d H:i:s') . "     User " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . " keeps messages in Junk folder for" . $jlast . ".\n");
+	$jlast = $purge_junk > 0 ? ($purge_junk == 1 ? " 1 day" : " " . strval($purge_junk) . " days") : "ever";
+	$rcmail->write_log('purgefolders', "User " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . " keeps messages in Junk folder for" . $jlast);
 	if (intval($mailboxes[$i]['purge_junk']) > 0) {
-		$junk = $rcmail_config['purge_maildir_path'] . "/" . $mailboxes[$i]['domain'] . "/" . $mailboxes[$i]['local_part'] . "/Maildir/.Junk/cur";
+		$junk = $rcmail->config->get('purge_maildir_path') . "/" . $mailboxes[$i]['maildir'] . "/.Junk/cur";
 		$files = array_diff(scandir($junk), array('..', '.'));
 		if (!empty($files)) {
 			foreach ($files as $filename) {
-				if ((time() - filemtime($junk . "/" . $filename)) > ($mailboxes[$i]['purge_junk'] * 60 * 60 * 24)) {
-					fwrite($log, date('Y-m-d H:i:s') . "     Purging file " . $filename . " (dated " . date('Y-m-d H:i:s', filemtime($trash . "/" . $filename)) . ") from Junk folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . ".\n");
+				if ((time() - filemtime($junk . "/" . $filename)) > ($purge_junk * 60 * 60 * 24)) {
+					$rcmail->write_log('purgefolders', "Purging file " . $filename . " (dated " . date('Y-m-d H:i:s', filemtime($trash . "/" . $filename)) . ") from Junk folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain']);
 					unlink($junk . "/" . $filename);
 					$jcount++;
 					}
 				}
 			}
 		}
-	fwrite($log, date('Y-m-d H:i:s') . "       " . $jcount . " messages purged from Junk folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain'] . ".\n");
+	$rcmail->write_log('purgefolders', $jcount . " messages purged from Junk folder of user " .  $mailboxes[$i]['local_part'] . "@" . $mailboxes[$i]['domain']);
 	}
 
-fwrite($log, date('Y-m-d H:i:s') . " - Purge operation ended.\n\n");
-fclose($log);
-
-$sql_result->free();
-$db->disconnect();
-
+$rcmail->write_log('purgefolders', "Purge operation ended");
 ?>
